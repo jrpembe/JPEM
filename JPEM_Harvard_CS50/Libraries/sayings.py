8f200803683c21ea1f2bb1f2702af12aacaa6c86
Jason Pemberton <jrpembe@mac.com>
2024-02-07 14:44:51 -0700

    JPEM_7February2024

--- JPEM_Harvard_CS50/Libraries/sayings.py
+++ JPEM_Harvard_CS50/Libraries/sayings.py
@@ -8,7 +8,11 @@ def hello(name):
 
 def goodbye(name):
     print(f"Goodbye, {name}")
-    
-if __name__ == "__main()__":  # This will prevent main from being called if this file is imported by another file
+
+# This variable __name__ value is automatically set by python to "main" when you run a file from a command line
+# when importing a file "main" will not get called by definition.
+
+if __name__ == "__main__":  
     main()
+
 
---
+++ JPEM_Harvard_CS50/Unit Tests/test_calculator.py
@@ -0,0 +1,41 @@
+from calculator import square
+
+# def main():
+#     test_square()
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+    
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+    
+def test_zero():
+    assert square(0) == 0
+    
+# Understanding Unit Test the hard way
+# def test_square():
+#     try:
+#         assert square(2) == 4
+#     except:
+#         print("2 squared was not 4")
+#     try:
+#         assert square(3) == 9
+#     except AssertionError:
+#         print("3 squared was not 9")
+#     try:
+#         assert square(-2) == 4
+#     except AssertionError:
+#         print("-2 squared was not 4")  
+#     try:
+#         assert square(-3) == 9
+#     except AssertionError:
+#         print("-3 squared was not 9") 
+#     try:
+#         assert square(0) == 0
+#     except AssertionError:
+#         print("0 squared was not 0")   
+        
+if __name__ == "__main__":
+    main()
