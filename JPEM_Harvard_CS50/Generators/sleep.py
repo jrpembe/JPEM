71906cf1bb224ba8f13e2eb80a15ab160329dd6d
Jason Pemberton <jrpembe@mac.com>
2024-03-08 09:23:36 -0700

    JPEM_08March2024

--- JPEM_Harvard_CS50/Generators/sleep.py
+++ JPEM_Harvard_CS50/Generators/sleep.py
@@ -4,17 +4,24 @@ def main():
         print(s)
 
 """
-this helper function is great for small numbers but at larger "n" this can excede your computers memory
-we can define a function as a generator to avoid this. The generator wil lreturn a little bit of data at a time
+this helper function is great for small numbers, but at larger "n" this can excede your computer's memory.
+We can define a function as a generator to avoid this. The generator will return a little bit of data at a time
 to do this we need "yield" instead of "return"
 """
 
+# def sheep(n):
+#     flock = []
+#     for i in range(n):
+#         flock.append("sheep" * i) 
+#     return flock
+
+
+# alternatively using yield
+""" "yield" returns one value for you for each iteration (each value of i), rather than "return" which runs the whole process before returning anything"""
+
 def sheep(n):
-    flock = []
     for i in range(n):
-        flock.append("sheep" * i) 
-    return flock
+        yield "sheep" * i
 
 if __name__ == "__main__":
     main()
-    
